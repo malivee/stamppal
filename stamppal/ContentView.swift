979daa979ea221bbb76c55f560a0cb5b8ae4a244
malivee <muhalif7123@gmail.com>
2026-09-03 08:19:25 +0700

    initial commit

---
+++ stamppal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  stamppal
+//
+//  Created by Muhammad Alief Rahman Fardillah on 03/09/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ stamppal/stamppalApp.swift
@@ -0,0 +1,17 @@
+//
+//  stamppalApp.swift
+//  stamppal
+//
+//  Created by Muhammad Alief Rahman Fardillah on 03/09/26.
+//
+
+import SwiftUI
+
+@main
+struct stamppalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
