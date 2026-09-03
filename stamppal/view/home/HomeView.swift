093055e9c369532cf2dc71986f4409b75361f641
malivee <muhalif7123@gmail.com>
2026-09-03 08:43:49 +0700

    minor fix

---
+++ stamppal/view/home/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  stamppal
+//
+//  Created by Muhammad Alief Rahman Fardillah on 03/09/26.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
